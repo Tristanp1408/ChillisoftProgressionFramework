62b67ac2024871ae8782e0e31b9e1b1e2ed4a783
Tristan Perumalsamy <tristan.perumalsamy@chillisoft.co.za>
2024-02-20 15:06:12 +0200

    Added Generics

---
+++ Level - 0/Generics/Generics/Box.cs
@@ -0,0 +1,16 @@
+﻿namespace Generics;
+
+public class Box<T>
+{
+    private readonly T _data;
+
+    public Box(T data)
+    {
+        _data = data;
+    }
+
+    public T GetData()
+    {
+        return _data;
+    }
+}
---
+++ Level - 0/Generics/Generics/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var intBox = new Box<int>(10);
+            Console.WriteLine("Integer value in the box: " + intBox.GetData());
+                
+            var stringBox = new Box<string>("Hello, Generics!");
+            Console.WriteLine("String value in the box: " + stringBox.GetData());
+
+            var boolBox = new Box<bool>(true);
+            Console.WriteLine("Boolean value in the box: " + boolBox.GetData());
+
+            var genericList = new List<int>{ 1, 2, 3, 4 };
+            genericList.Add(5);
+
+            genericList.Remove(2);
+
+            foreach (var number in genericList)
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
