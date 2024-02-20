839d5bb8386f187c219c30175cb95616fb3a12bb
Tristan Perumalsamy <tristan.perumalsamy@chillisoft.co.za>
2024-02-20 14:16:12 +0200

    demonstrated classes

---
+++ Level - 0/Classes/Classes/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car("Toyota", "Yaris", 2007);
+
+            car.DisplayInfo();
+        }
+    }
+}
